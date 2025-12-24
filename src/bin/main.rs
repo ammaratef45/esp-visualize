dfbdd64888acd4f7f6280c5c9a629db910b15925
Ammar Hussein <ammaratef45@proton.me>
2025-12-24 07:38:03 -0800

    add peripherals_extraction function

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -26,6 +26,8 @@ const COLS: usize = 64;
 const BITS: u8 = 4;
 const NROWS: usize = compute_rows(ROWS);
 const FRAME_COUNT: usize = compute_frame_count(BITS);
+const SSID: &str = env!("WIFI_SSID");
+const PASSWORD: &str = env!("WIFI_PASSWORD");
 
 type FBType = DmaFrameBuffer<ROWS, COLS, NROWS, BITS, FRAME_COUNT>;
 
@@ -37,6 +39,8 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
+// TODO: do I need heap allocator? and why?
+// https://github.com/Nereuxofficial/nostd-wifi-lamp/blob/main/src/main.rs
 
 #[allow(
     clippy::large_stack_frames,
@@ -50,13 +54,44 @@ fn main() -> ! {
     let peripherals = esp_hal::init(config);
     let (_, tx_descriptors) = esp_hal::dma_descriptors!(0, FBType::dma_buffer_size_bytes());
 
-    let mut matrix_display = WaveShare64X32Display::new(peripherals, tx_descriptors);
+    let hub75 = peripherals_extraction(peripherals, tx_descriptors);
+
+    let mut matrix_display = WaveShare64X32Display::new(hub75);
 
     loop {
         matrix_display = matrix_display.draw("Hi Again!");
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v~1.0/examples
+}
+
+fn peripherals_extraction<'a>(peripherals: Peripherals, tx_descriptors: &'static mut [DmaDescriptor]) -> Hub75<'a, Async> {
+    // https://learn.adafruit.com/adafruit-matrixportal-s3/pinouts
+    let hub75_pins = Hub75Pins16 {
+        red1: peripherals.GPIO42.degrade(),
+        grn1: peripherals.GPIO41.degrade(),
+        blu1: peripherals.GPIO40.degrade(),
+        red2: peripherals.GPIO38.degrade(),
+        grn2: peripherals.GPIO39.degrade(),
+        blu2: peripherals.GPIO37.degrade(),
+        addr0: peripherals.GPIO45.degrade(),
+        addr1: peripherals.GPIO36.degrade(),
+        addr2: peripherals.GPIO48.degrade(),
+        addr3: peripherals.GPIO35.degrade(),
+        addr4: peripherals.GPIO21.degrade(),
+        // MTX_OE
+        blank: peripherals.GPIO14.degrade(),
+        clock: peripherals.GPIO2.degrade(),
+        latch: peripherals.GPIO47.degrade(),
+    };
+    let hub75 = Hub75::new_async(
+        peripherals.LCD_CAM,
+        hub75_pins,
+        peripherals.DMA_CH0,
+        tx_descriptors,
+        Rate::from_mhz(20),
+    ).expect("failed to create Hub75!");
+    hub75
 }
 
 // https://www.waveshare.com/rgb-matrix-p2.5-64x32.htm
@@ -66,32 +101,7 @@ struct WaveShare64X32Display<'a> {
 }
 
 impl<'a> WaveShare64X32Display<'a> {
-    pub fn new(peripherals: Peripherals, tx_descriptors: &'static mut [DmaDescriptor]) -> Self {
-        // https://learn.adafruit.com/adafruit-matrixportal-s3/pinouts
-        let pins = Hub75Pins16 {
-            red1: peripherals.GPIO42.degrade(),
-            grn1: peripherals.GPIO41.degrade(),
-            blu1: peripherals.GPIO40.degrade(),
-            red2: peripherals.GPIO38.degrade(),
-            grn2: peripherals.GPIO39.degrade(),
-            blu2: peripherals.GPIO37.degrade(),
-            addr0: peripherals.GPIO45.degrade(),
-            addr1: peripherals.GPIO36.degrade(),
-            addr2: peripherals.GPIO48.degrade(),
-            addr3: peripherals.GPIO35.degrade(),
-            addr4: peripherals.GPIO21.degrade(),
-            // MTX_OE
-            blank: peripherals.GPIO14.degrade(),
-            clock: peripherals.GPIO2.degrade(),
-            latch: peripherals.GPIO47.degrade(),
-        };
-        let hub75 = Hub75::new_async(
-            peripherals.LCD_CAM,
-            pins,
-            peripherals.DMA_CH0,
-            tx_descriptors,
-            Rate::from_mhz(20),
-        ).expect("failed to create Hub75!");
+    pub fn new(hub75: Hub75<'a, Async>) -> Self {
         let fb = FBType::new();
         Self {
             hub75, fb
